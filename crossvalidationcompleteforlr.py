ea83a27ed3a507d809fc114ba7dd2a7ad27966fb
Chelz <chellam.ramanujadasan@gmail.com>
2016-06-07 10:42:10 -0500

    Create crossvalidationcompleteforlr.py

---
+++ crossvalidationcompleteforlr.py
@@ -0,0 +1,65 @@
+#Cross Validation Code for Logistic Regression
+def CrossValidationforLR(k):
+  countexcess={}
+  countunder={}
+  #Split the training data into k folds(train is the complete training data)
+  trainint=(train.coalesce(k,shuffle=True).glom().collect())
+  newfolds=[]
+  excess=[]
+  nSamplesinfold=len(train)/k
+#This code snippet ensures that each fold contains equal number of examples(nSamplesinfold here)
+  for i,val in enumerate(trainint):
+      if len(val)-nSamplesinfold>0:
+          countexcess[i]=len(val)-nSamplesinfold
+          trainint[i]=val[countexcess[i]:]
+          excess.append(val[:countexcess[i]+1])
+      else:
+          countunder[i]=nSamplesinfold-len(val)
+  excess=list(itertools.chain(*excess))
+  for key,value in countunder.items():
+      ex=excess[:value]
+      trainint[key].extend(ex)
+  accuracy_nb_val=[]
+  accuracy_nb_test=[]
+  for i in range(0,k):
+      print ("Iteration no. {}".format(i+1))
+#Ensures that one fold is used for validation and others are used for training(without replacement i.e one fold which was earlier 
+#used for validation is never used again)   
+      val=trainint[i]
+      train2=trainint[:i]
+      train2.extend(trainint[i+1:])
+      train21=list(itertools.chain(*train2))
+#form the training and validation data
+      train=sc.parallelize(train21)
+      validation=sc.parallelize(val)
+      hashingTF = HashingTF()
+#Feature Extraction for training data
+      tf_train = train.map(lambda tup: hashingTF.transform(tup[1]))
+      idf_train = IDF().fit(tf_train)
+      tfidf_train = idf_train.transform(tf_train)
+#feature Extraction for Validation Data
+      tf_val = validation.map(lambda tup: hashingTF.transform(tup[1]))
+      idf_val = IDF().fit(tf_val)
+      tfidf_val = idf_train.transform(tf_val)
+#Feature Extraction for test data
+      tf_test = test.map(lambda tup: hashingTF.transform(tup[1]))
+      idf_test = IDF().fit(tf_test)
+      tfidf_test = idf_test.transform(tf_test)
+#Labeled Data for training data
+      labels=train.map(lambda x:x[0])
+      transformeddata=labels.zip(tfidf_train)
+      labeled1_train = transformeddata.map(lambda k: LabeledPoint(k[0], k[1]))
+#Labeled Data for validation data
+      labelsval=validation.map(lambda x:x[0])
+      transformedvaldata=labelsval.zip(tfidf_val)
+      labeled1_val = transformedvaldata.map(lambda k: LabeledPoint(k[0], k[1]))
+#Labeled Data for Test Data
+      labelstest=test.map(lambda x:x[0])
+      transformeddatatest=labelstest.zip(tfidf_test)
+      labeled1_test = transformeddatatest.map(lambda k: LabeledPoint(k[0], k[1]))
+      model = LogisticRegressionWithSGD.train(labeled1_train,regParam=0.1)
+      predictionAndLabeltrainsgd = labeled1_train.map(lambda p: (model.predict(p.features), p.label))
+      accuracytrainsgd = 1.0 * predictionAndLabeltrainsgd.filter(lambda x: x[0] == x[1]).count() / train.count()
+      print('model Train accuracy for Logistic Regression with SGD {}'.format(accuracytrainsgd))
+      accuracy_sgd_train.append(accuracytrainsgd)
+  
